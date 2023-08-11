5344105a31257200f3ec31f39b574d4c660b4cda
JuanManuelSilva758 <juanmanuelsilva758@gmail.com>
2023-08-10 23:30:18 -0300

    subiendo el login

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() {}
+
+    
+     ngOnInit(): void {
+  }
+//   const btnSignIn =document.getElementById( "sign-in") ,
+//   btnSignUp =document.getElementById( "sign-up"), 
+//   formRegister=document.querySelector(".register") ,
+//   formLogin=document.querySelector(".login");
+
+//  btnSignIn.addEventListener("click",e => {
+
+// formRegister.classList.add("hide");
+// formLogin.classList.remove("hide");
+// })
+
+// btnSignUp.addEventListener("click",e =>{
+// formLogin.classList.add("hide");
+// formRegister?.classList.remove("hide")
+
+
+// })
+
+
+}
+
+
