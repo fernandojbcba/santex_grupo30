fbf45853730d95fc787617f8a15111f6779a42b4
Vicentetw <perrottavicente@gmail.com>
2023-10-09 17:38:50 -0300

    Create order payment Front

---
+++ front/src/app/core/services/payment/payment-service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PaymentService {
+    private apiUrl = environment.API_URL;
+
+  constructor(private http: HttpClient) {}
+
+  initiatePayment(courseprice: number): Observable<string> {
+    const url = `${this.apiUrl}/payments/create-order`;
+    
+    // Envía una solicitud al backend para iniciar el proceso de pago
+    return this.http.post<string>(url, { courseprice });
+
+  }
+}
--- front/src/app/modules/dashboard/my-courses/my-courses.component.ts
+++ front/src/app/modules/dashboard/my-courses/my-courses.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../core/services/user/user.service'
-
+import { Course } from 'src/app/core/interfaces/courses/course.interface';
+import { PaymentService } from 'src/app/core/services/payment/payment-service';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-my-courses',
@@ -10,20 +13,45 @@ import { UserService } from '../../../core/services/user/user.service'
 export class MyCoursesComponent implements OnInit {
   myCourses: any[] = [];
   displayedColumns: string[] = ['title', 'description','daysandhr','duration','estado', 'cancel']; 
-  constructor(private http:UserService) { }
+  constructor(private http:UserService,private router: Router, private authService: AuthService, private paymentService: PaymentService) { }
 
   ngOnInit(): void {
     this.http.get<any>('/courses/enrolled/:id').subscribe(
-      data => {
+      (data) => {
         this.myCourses=data;
         console.log(data);
       },
       error => {
-       
+        console.error('Error al obtener cursos:', error);
       }
     );
     
   }
- 
+  payForCourse(course: Course) {
+    if (course.price !== undefined) {
+      const courseprice: number = course.price;
+      console.log('Datos de la solicitud de pago:', courseprice);
+      // Llama al servicio de pago para iniciar el proceso de pago
+      this.paymentService.initiatePayment(courseprice).subscribe(
+        (response: any) => {
+          if (response && response.order) {
+            const orderUrl = response.order;
+            console.log('URL de pago recibida:', orderUrl);
+            // Redirige al usuario a la página de pago de MercadoPago
+            window.location.href = orderUrl;
+          } else {
+            console.error('Respuesta de pago incorrecta:', response);
+            // Handle the case where the response does not contain the expected order URL.
+            // You can display an error message to the user or take appropriate action.
+          }
+        },
+        (error: any) => {
+          console.error('Error al iniciar el proceso de pago:', error);
+          alert('Error al iniciar el proceso de pago. Por favor, inténtalo más tarde.');
+        }
+      );
+      
+  }
+  
 }
-
+}
