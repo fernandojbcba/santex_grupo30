d5eb75f6b35bc4d7687c17c13e549b1dadedc270
fernandojbcba <benavidez.fernando@hotmail.com>
2023-10-11 22:05:20 -0300

    fix boton baja, y modif perfil

--- front/src/app/modules/dashboard/my-courses/my-courses.component.ts
+++ front/src/app/modules/dashboard/my-courses/my-courses.component.ts
@@ -9,14 +9,14 @@ import { UserService } from '../../../core/services/user/user.service'
 })
 export class MyCoursesComponent implements OnInit {
   myCourses: any[] = [];
-  displayedColumns: string[] = ['title', 'description','daysandhr','duration','estado', 'cancel']; 
+  displayedColumns: string[] = ['title', 'description','daysandhr','duration','estado', ]; //'cancel'
   constructor(private http:UserService) { }
 
   ngOnInit(): void {
     this.http.get<any>('/courses/enrolled/:id').subscribe(
       data => {
         this.myCourses=data;
-        console.log(data);
+        
       },
       error => {
        
